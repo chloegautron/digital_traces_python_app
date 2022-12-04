322bbf69742f01e0c6087dc30ab275cd9b1b15e8
cgautron <chloe.gautron@epfedu.fr>
2022-12-04 15:09:00 +0100

    final commit

--- main.py
+++ main.py
@@ -3,5 +3,21 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
+
 def hello_world():
-    return "Hello World"
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async
+    src="https://www.googletagmanager.com/gtag/js?id=UA-250986685-2"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', ' UA-250986685-2');
+    </script>
+    """
+    return prefix_google + "Hello World"
+
+
+#def hello_world():
+#    return "Hello World"
