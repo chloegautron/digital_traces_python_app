55529794faed31e69d39fa7c7005b3898304367b
cgautron <chloe.gautron@epfedu.fr>
2022-12-21 15:14:18 +0100

    TP2 first commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import logging
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
@@ -8,16 +10,28 @@ def hello_world():
     prefix_google = """
     <!-- Google tag (gtag.js) -->
     <script async
-    src="https://www.googletagmanager.com/gtag/js?id=UA-250986685-2"></script>
+    src="https://www.googletagmanager.com/gtag/js?id=G-L4MNLKXJDK"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', ' UA-250986685-2');
+    gtag('config', 'G-L4MNLKXJDK');
     </script>
     """
     return prefix_google + "Hello World"
 
+@app.route('/logger', methods=['GET','POST'])
 
-#def hello_world():
-#    return "Hello World"
+def printMsg():
+    app.logger.warning('testing warning log')
+    app.logger.error('testing error log')
+    app.logger.info('testing info log')
+    return "Check your console"
+
+@app.route('/cookie', methods=['GET', 'POST'])
+def cookie():
+    req=requests.get('https://www.google.com/')
+    return req.cookies.get_dict()
+
+if __name__ == '__main__':
+    app.run(debug=True)
